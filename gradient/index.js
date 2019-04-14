fb0a8f5875567ee3447d50081c975f09d24142b5
Akseli Palen <akseli.palen@gmail.com>
2019-04-14 23:33:34 +0300

    Gradient app

--- gradient/index.js
+++ gradient/index.js
@@ -43,16 +43,25 @@ var main = function () {
     //   var w = canvas.width;
     //   var h = canvas.height;
     //
-    model.hue = (model.hue + 1) % 360;
+    model.hue = (model.hue + 10 * dt) % 360;
   };
 
   // View is rendered each frame
   var tickView = function (model) {
     // Draw; View current model
 
-    var h = model.hue.toFixed(0)
+    var w = canvas.width
+    var h = canvas.height
+
+    var hue0 = model.hue.toFixed(0)
+    var hue1 = ((model.hue + 50) % 360).toFixed(0)
+
+    var lg = ctx.createLinearGradient(0, 0, w, h)
+    lg.addColorStop(0, 'hsla(' + hue0 + ', 100%, 50%)')
+    lg.addColorStop(1, 'hsla(' + hue1 + ', 100%, 50%)')
+
     // ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'hsla(' + h + ', 100%, 50%)';
+    ctx.fillStyle = lg;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
   };
 
