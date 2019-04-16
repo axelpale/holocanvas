e609e0bf004bf42616d24422448518ef02fd871c
Akseli Palen <akseli.palen@gmail.com>
2019-04-17 00:09:46 +0300

    Convert mono-color to use the holocanvas module

--- mono-color/index.js
+++ mono-color/index.js
@@ -1,108 +1,25 @@
+// Mono color animation
 
-var makeCanvasAutoFullwindow = function (canvas) {
-  // Canvas is resized when window size changes, e.g.
-  // when a mobile device is tilted.
-  //
-  // Parameter
-  //   canvas
-  //     HTML Canvas element
-  //
-  var resizeCanvas = function () {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  };
-  // resize the canvas to fill browser window dynamically
-  window.addEventListener('resize', resizeCanvas, false);
-  // Initially resized to fullscreen.
-  resizeCanvas();
-};
+// Everything is drawn on canvas.
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
 
-var makeCursorDisappear = function (el) {
-  // Hides cursor after one second and reveals it on mouse move
-  //
-  // Parameter
-  //   el
-  //     HTML Element
-  //
-  var timeout = null;
+// Make canvas resize automatically to full window area
+holocanvas.makeCanvasAutoFullwindow(canvas);
+holocanvas.makeCursorDisappear(canvas);
 
-  var hideCursor = function () {
-    el.style.cursor = 'none'
-  }
-
-  var showCursor = function () {
-    window.clearTimeout(timeout)
-    timeout = setTimeout(hideCursor, 1000)
-    el.style.cursor = 'auto'
-  };
-
-  el.addEventListener('mousemove', showCursor, false);
-};
-
-var main = function () {
-  var c = document.getElementById('canvas');
-  // Everything is drawn on canvas.
-  var ctx = canvas.getContext('2d');
-  // Make canvas resize automatically to full window area
-  makeCanvasAutoFullwindow(canvas);
-  makeCursorDisappear(canvas);
-
-  var initModel = function () {
-    // Returns the model.
-    // The model is updated using tickModel.
-    // The model is visualized using tickView.
+holocanvas.start({
+  initModel: function () {
     return {
       hue: 0 // degrees
     }
-  }
-
-  // Model is simulated forward every frame
-  var tickModel = function (model, dt) {
-    // Parameter
-    //   dt
-    //     simulation time, seconds
-    //
-    // If needed, you can access the canvas dimensions:
-    //   var w = canvas.width;
-    //   var h = canvas.height;
-    //
+  },
+  tickModel: function (model, dt) {
     model.hue = (model.hue + 1) % 360;
-  };
-
-  // View is rendered each frame
-  var tickView = function (model) {
-    // Draw; View current model
-
-    var h = model.hue.toFixed(0)
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+  },
+  tickView: function (model) {
+    var h = model.hue.toFixed(0);
     ctx.fillStyle = 'hsla(' + h + ', 100%, 50%)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-  };
-
-  // number, unix timestamp milliseconds of most recent frame.
-  var past = null;
-  // Visualization state
-  var model = initModel();
-
-  var startAnimationLoop = function loopFn() {
-    var present, dt;
-
-    // Time difference from previous frame in milliseconds
-    present = Date.now();
-    dt = (past === null) ? 0 : present - past;
-    past = present;
-
-    // Update Model
-    tickModel(model, dt / 1000); // secs
-
-    // Draw; View current model
-    tickView(model);
-
-    // Recursion
-    window.requestAnimationFrame(loopFn);
-  };
-
-  startAnimationLoop()
-}
-
-main();
+  }
+});
