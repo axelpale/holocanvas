e3a14a1061edfd4bc198ea73373c6e0cb84fe9cc
Akseli Palen <akseli.palen@gmail.com>
2019-04-17 00:26:30 +0300

    Convert rolling-rainbow to use the holocanvas module

--- rolling-rainbow/index.js
+++ rolling-rainbow/index.js
@@ -1,92 +1,30 @@
+// Rolling rainbow hologram animation
 
-var makeCanvasAutoFullwindow = function (canvas) {
-  // Canvas is resized when window size changes, e.g.
-  // when a mobile device is tilted.
-  //
-  // Parameter
-  //   canvas
-  //     HTML Canvas element
-  //
-  var resizeCanvas = function () {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  };
-  // resize the canvas to fill browser window dynamically
-  window.addEventListener('resize', resizeCanvas, false);
-  // Initially resized to fullscreen.
-  resizeCanvas();
-};
+// Everything is drawn on canvas.
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
 
-var makeCursorDisappear = function (el) {
-  // Hides cursor after one second and reveals it on mouse move
-  //
-  // Parameter
-  //   el
-  //     HTML Element
-  //
-  var timeout = null;
+// Make canvas resize automatically to full window area
+holocanvas.makeCanvasAutoFullwindow(canvas);
+holocanvas.makeCursorDisappear(canvas);
 
-  var hideCursor = function () {
-    console.log('hide')
-    el.style.cursor = 'none'
-  }
-
-  var showCursor = function () {
-    console.log('show')
-    window.clearTimeout(timeout)
-    timeout = setTimeout(hideCursor, 1000)
-    el.style.cursor = 'auto'
-  };
-
-  el.addEventListener('mousemove', showCursor, false);
-};
-
-var main = function () {
-  var c = document.getElementById('canvas');
-  // Everything is drawn on canvas.
-  var ctx = canvas.getContext('2d');
-  // Make canvas resize automatically to full window area
-  makeCanvasAutoFullwindow(canvas);
-  makeCursorDisappear(canvas);
-
-  var initModel = function () {
-    // Returns the model.
-    // The model is updated using tickModel.
-    // The model is visualized using tickView.
+holocanvas.start({
+  initModel: function () {
     return {
-      // Note: do not write latent variables here.
-      //       They should be created on runtime.
-      hue: 300,
+      hue: 300, // degrees
       dhue: 36,
       y: 0,
       distance: 6,
       speed: 2000
     }
-  }
-
-  // Model is simulated forward every frame
-  var tickModel = function (model, dt) {
-    // Parameter
-    //   dt
-    //     simulation time, seconds
-    //
-    // If needed, you can access the canvas dimensions:
-    //   var w = canvas.width;
-    //   var h = canvas.height;
-    //
-
-    // Source variables
+  },
+  tickModel: function (model, dt) {
+    // Update source variables.
     model.dy = model.speed * dt;
-    //model.hue0 = (model.hue0 + 50 * dt) % 360;
+    // Update latent variables.
     model.y = (model.y + model.dy) % ((model.distance + 1) * canvas.height);
-
-    // Latent variables
-  };
-
-  // View is rendered each frame
-  var tickView = function (model) {
-    // Draw; View current model
-
+  },
+  tickView: function (model) {
     var w = canvas.width
     var h = canvas.height
 
@@ -125,36 +63,8 @@ var main = function () {
     lg.addColorStop(1.00, 'hsla(' + hue + ', 100%, 0%)')
 
     ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
+    ctx.fillRect(0, 0, w, h);
     ctx.fillStyle = lg;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-  };
-
-  // number, unix timestamp milliseconds of most recent frame.
-  var past = null;
-  // Visualization state
-  var model = initModel();
-
-  var startAnimationLoop = function loopFn() {
-    var present, dt;
-
-    // Time difference from previous frame in milliseconds
-    present = Date.now();
-    dt = (past === null) ? 0 : present - past;
-    past = present;
-
-    // Update Model
-    tickModel(model, dt / 1000); // secs
-
-    // Draw; View current model
-    tickView(model);
-
-    // Recursion
-    window.requestAnimationFrame(loopFn);
-  };
-
-  startAnimationLoop()
-}
-
-main();
+    ctx.fillRect(0, 0, w, h);
+  }
+});
